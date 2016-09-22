aa05a822f1c4bb34432a46afa4d61e4a6faf87a9
Henrique Jensen <henriquejensen@hotmail.com>
2016-09-22 10:25:19 -0300

    ajuste

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -76,7 +76,7 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
   config.assets.precompile += Ckeditor.assets
-  config.assets.precompile += %w( ckeditor/* )
+  config.assets.precompile += %w(ckeditor/* )
   config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
