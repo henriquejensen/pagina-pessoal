6789cefe84f3fd79752fa0bd65f9b79227cdb257
Henrique Jensen <henriquejensen@hotmail.com>
2016-09-22 01:02:13 -0300

    novas configs carrierwave

--- Gemfile
+++ Gemfile
@@ -3,6 +3,9 @@ source 'https://rubygems.org'
 ruby "2.3.0"
 
 gem 'ckeditor', '~> 4.1'
+gem 'carrierwave'
+gem 'mini_magick'
+
 gem 'simple_form', '~> 3.2'
 gem 'bootstrap-sass', '~> 3.3'
 
--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,7 @@
+config.assets.precompile += Ckeditor.assets
+config.assets.precompile += %w( ckeditor/* )
+config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+
 require_relative 'boot'
 
 require 'rails/all'
